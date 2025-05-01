8ac3dc9ae6beafd48eca747865f80b36b1be3b10
vpolisettiFerroglobe <vpolisetti@ferroglobe.com>
2025-04-30 20:33:03 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_RsNiZ6FFZ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4qf080h5r63l60kmilufs9u95d', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
